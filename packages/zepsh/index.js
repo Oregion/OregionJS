7a5411029da3aac8e321cb97a9b6d07de58ebbc5
itsZepsh <zepsh@ibrahimstudio.com>
2025-05-24 17:33:58 +0700

    ops: update the public API to export new utilities and components

--- packages/zepsh/index.js
+++ packages/zepsh/index.js
@@ -2,11 +2,13 @@
  * @module zepsh
  * Public API for the Zepsh framework.
  */
-import { createElement, Fragment, Suspense, ErrorBoundary, memo, Profiler, StrictMode, ViewTransition } from "./src/element";
+import { createElement, Fragment, Suspense, ErrorBoundary, memo, Profiler, StrictMode, ViewTransition, Activity, cloneElement, isValidElement, createRef } from "./src/element";
 import { createContext } from "./src/context";
 import { act } from "./src/testing";
 import { useState, useEffect, useLayoutEffect, useInsertionEffect, useRef, useRefCleanup, useImperativeHandle, useMemo, useCallback, useReducer, useContext, useDeferredValue, useTransition, useActionState, useOptimistic, useSyncExternalStore, useId, useDebugValue, useErrorBoundary, useLocalStorage, useDebounce, useFetch, use, startTransition, cache } from "./src/hooks";
 import { captureOwnerStack, mergeRefs } from "./src/utils";
+import { createFormScope } from "./src/form";
+import { Children } from "./src/children";
 import { lazy } from "./src/lazy";
 
 export const Hooks = {
@@ -42,11 +44,13 @@ export const Components = {
   Profiler,
   StrictMode,
   ViewTransition,
+  Activity,
 };
 
 export const APIs = {
   createElement,
   createContext,
+  createFormScope,
   memo,
   act,
   cache,
@@ -54,6 +58,10 @@ export const APIs = {
   mergeRefs,
   lazy,
   startTransition,
+  cloneElement,
+  isValidElement,
+  createRef,
+  Children,
 };
 
 export default {
